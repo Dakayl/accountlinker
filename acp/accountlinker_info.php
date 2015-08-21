2286b18a1716e937c7c047f344f0c6fb98191408
Nicolas Brouet <nicolas.brouet84@gmail.com>
2015-08-21 10:54:31 +0200

    Create accountlinker_info.php

---
+++ acp/accountlinker_info.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * @package phpBB Extension - Account Linker
+ * @author Dakayl
+ * @copyright (c) 2015 Dakayl
+ * @license GNU General Public License, version 3 (GPL-3.0)
+ *
+ */
+namespace dakayl\accountlinker\acp;
+/**
+* @package module_install
+*/
+class adduser_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> 'dakayl\accountlinker\acp\accountlinker_module',
+			'title'		=> 'ACCOUNT_LINKER',
+			'version'	=> '1.0.0',
+			'modes'		=> array(
+				'main'	=> array('title' => 'ACP_ACCOUNT_LINKER', 'auth'	=> 'ext_dakayl\accountlinker && acl_a_user', 'cat'	=> array('ACP_CAT_USERS')),
+			),
+		);
+	}
+	function install()
+	{
+	}
+	function uninstall()
+	{
+	}
+}
